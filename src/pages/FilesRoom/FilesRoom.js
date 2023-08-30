14fb8b4ca9a9a6d4a69d6128b78ed575dee176a0
TytusKolpak <tytus.kolpak.71@gmail.com>
2023-08-30 17:34:30 +0200

    New access group can be added to user (if exists)

--- src/pages/FilesRoom/FilesRoom.js
+++ src/pages/FilesRoom/FilesRoom.js
@@ -3,9 +3,13 @@ import './FilesRoom.css';
 
 // React variable handling
 import { useEffect, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 // for getting auth on reload
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -16,7 +20,7 @@ import { ref, getDownloadURL, uploadBytes, listAll } from "firebase/storage";
 
 // Database related
 import { firestore } from '../../firebase_setup/firebase';
-import { getDoc, doc } from 'firebase/firestore';
+import { getDoc, doc, updateDoc, arrayUnion } from 'firebase/firestore';
 
 function FilesRoom({ currentUser }) {
     const auth = getAuth();
@@ -24,6 +28,10 @@ function FilesRoom({ currentUser }) {
     const [selectedFile, setSelectedFile] = useState(null);
     const [listLoaded, setListLoaded] = useState(false);
     const [itemsByFolder, setItemsByFolder] = useState({});
+    const [selectedFolder, setSelectedFolder] = useState('Select folder to upload to');
+    const [accessGroups, setAccessGroups] = useState([]);
+    const [newGroup, setNewGroup] = useState('');
+    const [newGroupExists, setNewGroupExists] = useState(true);
 
     useEffect(() => {
         console.log("Initializing user email");
@@ -35,7 +43,7 @@ function FilesRoom({ currentUser }) {
         displayItems();
         // eslint-disable-next-line
     }, [])
-    
+
     async function displayItems() {
         // Access determination part
         console.log("Finding user-available folders");
@@ -57,6 +65,7 @@ function FilesRoom({ currentUser }) {
             // docSnap.data() will be undefined in this case
             console.log("No such document for", currentUserEmail, "!");
         }
+        setAccessGroups(accessGroups);
 
         const updatedItemList = [];
         const updatedItemsByFolder = {};
@@ -85,6 +94,7 @@ function FilesRoom({ currentUser }) {
                 console.log("Uh-oh, an error occurred!", error);
             }
         }
+        console.log("updatedItemsByFolder:", updatedItemsByFolder);
 
         setItemsByFolder(updatedItemsByFolder);
 
@@ -97,9 +107,8 @@ function FilesRoom({ currentUser }) {
 
     const handleUpload = (e) => {
         e.preventDefault();
-        if (!selectedFile) return;
 
-        const storageRef = ref(storage, `uploadedImages/${selectedFile.name}`);
+        const storageRef = ref(storage, selectedFolder + "/" + selectedFile.name);
 
         uploadBytes(storageRef, selectedFile)
             .then((snapshot) => {
@@ -110,8 +119,67 @@ function FilesRoom({ currentUser }) {
                 console.error('Error uploading file:', error);
             });
 
-        // displayItems(); somehow it should rerender the list
+        displayItems();
     };
+
+    const handleFolderSelect = (eventKey) => {
+        setSelectedFolder(eventKey);
+        console.log("Selected:", eventKey);
+    };
+
+    async function handleNewGroupSubmit(e) {
+        console.log("handleNewGroupSubmit");
+        e.preventDefault();
+
+        // Get storage groups document in storage access collection
+        const collectionName = "StorageAccess";
+        const documentIdentification = "StorageGroups";
+        const docRef = doc(firestore, collectionName, documentIdentification);
+
+        const docSnapshot = await getDoc(docRef);
+
+        const groups = docSnapshot.data().groups;
+        const hasNewGroup = groups.includes(newGroup);
+        
+        if (hasNewGroup) {
+            console.log("Specified group exists. Adding it to users access groups.");
+
+            // Add a group to user access groups
+            const documentIdentification = currentUserEmail;
+            const docRef = doc(firestore, collectionName, documentIdentification);
+
+            await updateDoc(docRef,{
+                accessGroups: arrayUnion(newGroup)
+            });
+
+            displayItems();
+        } else {
+            console.log("Specified group doesn't exist!");
+            setNewGroupExists(false)
+            // Turn off the visibility after 5 seconds
+            setTimeout(() => {
+                setNewGroupExists(true)
+            }, 5000);
+        }
+
+        // // Add a new group to current user in access group collection
+        // try {
+        //     const collectionName = "StorageAccess";
+        //     const documentIdentification = currentUserEmail;
+        //     const docRef = doc(firestore, collectionName, documentIdentification);
+        //     const docSnap = await getDoc(docRef);
+
+        //     if (docSnap) {
+        //         console.log("Adding an access group to current user");
+        //     }
+
+        // } catch (e) {
+        //     console.error("Error adding document: ", e);
+        // }
+
+        // Clear input field
+        setNewGroup('')
+    }
 
     return (
         <div className='filesRoom'>
@@ -124,17 +192,40 @@ function FilesRoom({ currentUser }) {
                         <Form.Group className="mb-3" controlId="input">
                             <Form.Control type="file" onChange={handleFileChange} />
                         </Form.Group>
-                        {selectedFile ?
-                            <Button variant="primary" type="submit">
-                                Upload
-                            </Button>
-                            :
-                            <Button variant="primary" type="submit" disabled>
-                                Upload
-                            </Button>
-                        }
 
+                        <DropdownButton
+                            variant='secondary'
+                            className='mb-3'
+                            title={selectedFolder}
+                            onSelect={handleFolderSelect}>
+                            {accessGroups.map((element, index) => (
+                                <Dropdown.Item eventKey={element} key={index}>{element}</Dropdown.Item>
+                            ))}
+                        </DropdownButton>
+
+                        <Button variant="primary" type="submit" disabled={(!selectedFolder || !selectedFile)}>
+                            Upload
+                        </Button>
                     </Form>
+
+                    <h4 className='mt-5'>Groups</h4>
+                    <div className='newContact'>
+                        <Form onSubmit={handleNewGroupSubmit}>
+                            <Form.Group className="mb-3">
+                                <Form.Control
+                                    placeholder="Enter group to join"
+                                    value={newGroup}
+                                    onChange={e => setNewGroup(e.target.value)}
+                                />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Add
+                            </Button>
+                        </Form>
+
+                        {!newGroupExists && <h5 className='warning'>Specified group doesn't exist!</h5>}
+
+                    </div>
                 </div>
 
                 <div className='displaying'>
@@ -143,7 +234,7 @@ function FilesRoom({ currentUser }) {
                     {listLoaded ?
                         <div className='horizontalFlex'>
                             {Object.entries(itemsByFolder).map(([folder, items]) => (
-                                <div className="accessGroup" key={folder}>
+                                <div className="singleFolder" key={folder}>
                                     <h6>{folder}</h6>
                                     <ListGroup>
                                         {items.map((item, index) => (
--- src/pages/NoPage/NoPage.js
+++ src/pages/NoPage/NoPage.js
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import "./NoPage.css"
+import Spinner from 'react-bootstrap/Spinner';
 
 const NoPage = () => {
+    const [displaySpinner, setDisplaySpinner] = useState(true);
+
+    const loadingTime = 5 //In seconds
+
+    useEffect(() => {
+        // Turn off the visibility after specified time
+        setTimeout(() => {
+            setDisplaySpinner(false)
+        }, loadingTime * 1000);
+    }, [])
+
     return (
         <div className="NoPage">
-            <h1>Error</h1>
-            <p>Either page doesn't exist or You have no permission to view it.</p>
+            {!displaySpinner ?
+                <>
+                    <h1>Error</h1>
+                    <p>Either page doesn't exist or You have no permission to view it.</p>
+                </>
+                :
+                <>
+                    <h1>Loading...</h1>
+                    <div className='center'>
+                        <Spinner animation="border" role="status"></Spinner>
+                    </div>
+                </>
+            }
         </div>
     );
 };
